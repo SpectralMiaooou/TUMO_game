7fff1fb34d30ba33200e9a438b107f6c17d7cf35
KarlissDorlin <65249896+SpectralMiaooou@users.noreply.github.com>
2023-04-18 23:18:53 +0200

    jour 15

--- TUMO_game_KD/Assets/Scripts/PlayerController.cs
+++ TUMO_game_KD/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,7 @@ public class PlayerController : MonoBehaviour
     CharacterController character;
 
     public float speed = 10f;
-    public float jumpForce = 10f;
     private Vector3 impact = Vector3.zero;
-    private float _gravity = -9.81f;
-    [SerializeField] private float gravityMultiplier = 3.0f;
-    private float vY;
     public Vector3 desiredMoveDirection;
 
     Vector2 move;
@@ -22,12 +18,26 @@ public class PlayerController : MonoBehaviour
 
     PlayerControls controls;
 
-    private bool isJumping;
     private bool isGrounded;
     private bool isAttacking;
 
     public Transform groundCheck;
     public LayerMask groundMask;
+
+    //Movement variables
+    public Vector3 currentMovement;
+
+    //Gravity variables
+    private float gravity = -9.81f;
+    private float groundedGravity = -0.05f;
+
+
+    //Jumping variables
+    private bool isJumpPressed = false;
+    private bool isJumping = false;
+    private float maxJumpHeight = 3f;
+    private float maxJumpTime = 1f;
+    private float initialJumpVelocity;
 
 
 
@@ -37,8 +47,18 @@ public class PlayerController : MonoBehaviour
 
         controls.Gameplay.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
         controls.Gameplay.Movement.canceled += ctx => move = Vector2.zero;
-        controls.Gameplay.Jump.performed += Jump;
+        controls.Gameplay.Jump.started += onJump;
+        controls.Gameplay.Jump.canceled += onJump;
         controls.Gameplay.Attack.performed += Attack;
+
+        setupJumpVariables();
+    }
+
+    void setupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
     }
 
     private void OnEnable()
@@ -66,13 +86,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = character.isGrounded;
-        anim.SetBool("isGrounded", isGrounded);
 
         Movement();
-        ApplyGravity();
-        //FallingCheck();
         //UpdateImpact();
+
+        character.Move(currentMovement * Time.deltaTime);
+
+        isGrounded = IsGrounded();
+        anim.SetBool("isGrounded", IsGrounded());
+
+        handleGravity();
+        handleJump();
     }
 
     void Movement()
@@ -80,8 +104,11 @@ public class PlayerController : MonoBehaviour
         if (!isAttacking)
         {
             anim.SetFloat("inputX", move.magnitude, 0.0f, Time.deltaTime * 2f);
+            currentMovement.x = 0f;
+            currentMovement.z = 0f;
+
             //print(moveDirection.magnitude);
-            if (move.magnitude < 0.4)
+            if (move.magnitude < 0.3)
             {
                 return;
             }
@@ -98,56 +125,63 @@ public class PlayerController : MonoBehaviour
             desiredMoveDirection = forward * move.y + right * move.x;
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), Time.deltaTime * 10f);
-		
-	        character.Move(transform.forward * move.magnitude * speed* Time.deltaTime);
+
+            desiredMoveDirection = transform.forward * move.magnitude * speed;
+
+            currentMovement.x = desiredMoveDirection.x;
+            currentMovement.z = desiredMoveDirection.z;
 
         }
     }
-    private void ApplyGravity()
+    private void handleGravity()
     {
-	    if(isGrounded && vY < 0.0f)
-	    {
-		    vY = 0f;
-	    }
-	    else
-	    {
-            vY += _gravity * Time.deltaTime;
-        }
-        character.Move(transform.up * vY);
-    }
+        bool isFalling = currentMovement.y <= 0.0f;
+        float fallMultiplier = 2.0f;
 
+        //anim.SetBool( "isFalling", isFalling);
 
-    void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && isGrounded)
+        if (IsGrounded())
         {
-            isGrounded = false;
-            vY += jumpForce;
+            currentMovement.y = groundedGravity;
+	    }
+        else if(isFalling)
+        {
+            float previousYVelocity = currentMovement.y;
+            float newYVelocity = currentMovement.y + (gravity * fallMultiplier * Time.deltaTime);
+            float nextYVelocity = Mathf.Max((previousYVelocity + newYVelocity) * 0.5f,-20.0f) ;
+            currentMovement.y = nextYVelocity;
+        }
+	    else
+        {
+            float previousYVelocity = currentMovement.y;
+            float newYVelocity = currentMovement.y + (gravity * Time.deltaTime);
+            float nextYVelocity = (previousYVelocity + newYVelocity) * 0.5f;
+            currentMovement.y = nextYVelocity;
+        }
+    }
 
+
+    void handleJump()
+    {
+        if (isJumpPressed && IsGrounded() && !isJumping)
+        {
             isJumping = true;
             anim.SetBool("isJumping", true);
+
+            currentMovement.y = initialJumpVelocity * 0.5f;
+        }
+        else if (!isJumpPressed && isJumping && IsGrounded())
+        {
+            isJumping = false;
+            anim.SetBool("isJumping", false);
         }
     }
     void Attack(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed && IsGrounded())
         {
             //isAttacking = true;
             anim.SetTrigger("isAttacking");
-        }
-    }
-
-    void FallingCheck()
-    {
-        if (vY < -0.1 && !isGrounded)
-        {
-            anim.SetBool("isFalling", true);
-            isJumping = false;
-            anim.SetBool("isJumping", false);
-        }
-        else
-        {
-            anim.SetBool("isFalling", false);
         }
     }
 
@@ -162,5 +196,20 @@ public class PlayerController : MonoBehaviour
         if (impact.magnitude > 0.2) character.Move(impact * Time.deltaTime);
         // consumes the impact energy each cycle:
         impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.05f, groundMask);
+    }
+
+
+
+
+
+
+    void onJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
     }
 }
